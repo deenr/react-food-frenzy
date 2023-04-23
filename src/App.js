04e513d1df320a4f8551395b0091d4b6c66b961c
Dean Reymen <deenrayman@outlook.com>
2023-04-23 21:52:12 +0300

    mobile styling

--- src/App.js
+++ src/App.js
@@ -15,32 +15,49 @@ function App() {
             Food Frenzy
           </Logo>
         </Nav>
-        <Search />
-        <Category />
-        <Pages />
+        <Content>
+          <Search />
+          <Category />
+          <Pages />
+        </Content>
       </BrowserRouter>
     </div>
   );
 }
 
+const Content = styled.div`
+  padding: 0 3rem;
+
+  @media (max-width: 960px) {
+    padding: 0 2rem;
+  }
+`;
+
 const Logo = styled(Link)`
   display: flex;
   justify-content: flex;
   align-items: center;
+  gap: 1rem;
   text-decoration: none;
   font-size: 1.5rem;
   font-weight: 400;
   font-family: "Lobster Two", cursive;
+  color: white;
 `;
 
 const Nav = styled.div`
-  padding: 4rem 0;
+  padding: 2rem 4rem;
   display: flex;
   justify-content: flex-start;
   align-items: center;
+  background-color: #52796f;
 
   svg {
     font-size: 2rem;
+  }
+
+  @media (max-width: 960px) {
+    padding: 1rem 2rem;
   }
 `;
 
--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -1,5 +1,4 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 
@@ -22,46 +21,58 @@ function Recipe() {
 
   return (
     <DetailWrapper>
-      <div>
+      <Presentation>
         <h2>{recipe?.title}</h2>
         <img src={recipe?.image} alt={recipe?.title} />
-      </div>
-      <Info>
-        <Button
-          className={activeTab === "instructions" ? "active" : ""}
-          onClick={() => setActiveTab("instructions")}
-        >
-          Instructions
-        </Button>
-        <Button
-          className={activeTab === "ingredients" ? "active" : ""}
-          onClick={() => setActiveTab("ingredients")}
-        >
-          Ingredients
-        </Button>
-        {activeTab === "instructions" ? (
-          <>
-            <div dangerouslySetInnerHTML={{ __html: recipe?.summary }}></div>
-            <div
-              dangerouslySetInnerHTML={{ __html: recipe?.instructions }}
-            ></div>
-          </>
-        ) : (
-          <ul>
-            {recipe.extendedIngredients.map((ingredient) => (
-              <li key={ingredient.id}>{ingredient.original}</li>
-            ))}
-          </ul>
-        )}
-      </Info>
+      </Presentation>
+      <InfoSection>
+        <InfoButton>
+          <Button
+            className={activeTab === "instructions" ? "active" : ""}
+            onClick={() => setActiveTab("instructions")}
+          >
+            Instructions
+          </Button>
+          <Button
+            className={activeTab === "ingredients" ? "active" : ""}
+            onClick={() => setActiveTab("ingredients")}
+          >
+            Ingredients
+          </Button>
+        </InfoButton>
+
+        <InfoDetails>
+          {activeTab === "instructions" ? (
+            <>
+              <div dangerouslySetInnerHTML={{ __html: recipe?.summary }}></div>
+              <div
+                dangerouslySetInnerHTML={{ __html: recipe?.instructions }}
+              ></div>
+            </>
+          ) : (
+            <ul>
+              {recipe.extendedIngredients.map((ingredient) => (
+                <li key={ingredient.id}>{ingredient.original}</li>
+              ))}
+            </ul>
+          )}
+        </InfoDetails>
+      </InfoSection>
     </DetailWrapper>
   );
 }
 
 const DetailWrapper = styled.div`
-  margin-top: 10rem;
+  margin-top: 4rem;
   margin-bottom: 5rem;
-  display: flex;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 2rem;
+
+  @media (max-width: 960px) {
+    grid-template-columns: auto;
+    grid-template-rows: auto auto;
+  }
 
   .active {
     background-color: #52796f;
@@ -76,23 +87,45 @@ const DetailWrapper = styled.div`
     font-size: 1.2rem;
     line-height: 2.5rem;
   }
-
-  ul {
-    margin-top: 2rem;
-  }
 `;
-
 const Button = styled.button`
   padding: 1rem 2rem;
   color: #52796f;
   background-color: white;
   border: 2px solid #52796f;
-  margin-right: 2rem;
   font-weight: 600;
+  width: 100%;
 `;
 
-const Info = styled.div`
-  margin-left: 10rem;
+const InfoSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+`;
+
+const InfoButton = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 2rem;
+
+  @media (max-width: 960px) {
+    justify-content: space-between;
+  }
+`;
+
+const InfoDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+`;
+
+const Presentation = styled.data`
+  overflow: hidden;
+
+  img {
+    width: 100%;
+    object-fit: cover;
+  }
 `;
 
 export default Recipe;
